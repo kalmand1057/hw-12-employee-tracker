35d06356d46fd9f9dd5a33218a22b56448849484
Kalena Almand <kalmand1057@gmail.com>
2020-10-14 01:13:00 -0500

    installed init inquirer and mysql packages

---
+++ server.js
@@ -0,0 +1,19 @@
+//Dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//Create connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "!057Captain",
+    database: "employeeTrackerDB"
+  });
+
+  //Make connection
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    //next function
+  });
