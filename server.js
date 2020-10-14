90e244e5b3897c45096e5c5f250297a285d36dd9
Kalena Almand <kalmand1057@gmail.com>
2020-10-14 01:17:41 -0500

    installed console table

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
 
 //Create connection
 var connection = mysql.createConnection({
